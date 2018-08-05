ad25fe22aa4f1f4a5058d24eaf210f48ee7824b4
lianxiaoju <670834836@qq.com>
2018-08-05 20:53:29 +0800

    sure product

--- lechun_client/src/mock/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import mockAdapter from 'axios-mock-adapter';
-import Data from './data/data.json'
-
-const mock = new mockAdapter(axios);
-mock.onGet('/data').reply(200, Data);
